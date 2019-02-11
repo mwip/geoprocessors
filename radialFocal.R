db6824ecad6568bf76f8dfba84701e1e10ff2205
Loki <matthias.weigand@protonmail.com>
2019-02-11 16:07:56 +0100

    Minor revisions of license headers

--- radialFocal.R
+++ radialFocal.R
@@ -1,5 +1,5 @@
 #! /usr/bin/Rscript --vanilla
-#    radielFocal - Calculate a radial focal filter for an image
+#    radialFocal - Calculate a radial focal filter for an image
 #    Copyright (C) 2019 Matthias Weigand -- matthias.weigand[at]protonmail.com
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@ option_list = list(
   make_option(c('-o', '--output'), type = 'character', default = NA, help = 'character. output data file, must be GeoTIFF'),
   make_option(c('-r', '--radius'), type = 'numeric', default = 100, help = 'numeric. radius in map units (defined by inputfile crs), default 100'),
   make_option(c('-f', '--fun'), type = 'character', default = "mean", help = 'character. specify the function you want to use, refer to: R -e "?raster::focal"'), 
-  make_option(c('-w', '--overwrite'), action = 'store_true', default = TRUE, type = 'logical', help = 'logical. should the output file be overwritten if necessary')
+  make_option(c('-w', '--overwrite'), action = 'store_true', default = FALSE, type = 'logical', help = 'logical. should the output file be overwritten if necessary')
 )
 opt_parser <- OptionParser(usage = "Usage: %prog -i input.tif -o output.tif -r 100 -f mean -overwrite", 
                            option_list=option_list)
--- tileWithOverlap.R
+++ tileWithOverlap.R
@@ -1,5 +1,5 @@
 #!/usr/bin/Rscript --vanilla
-#    radialFocal - Calculate a radial focal filter for an image
+#    tileWithOverlap - Tile an image into overlapping tiles
 #    Copyright (C) 2019 Matthias Weigand -- matthias.weigand[at]protonmail.com
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
